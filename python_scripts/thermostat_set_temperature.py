7694b97ff22469f349fda54c5fd41ca5b8dc3f2b
Unknown <cbestle@gmail.com>
2019-02-17 13:06:25 +0100

    rettelser

--- python_scripts/thermostat_set_temperature.py
+++ python_scripts/thermostat_set_temperature.py
@@ -1,10 +1,13 @@
 thermostat_id  = data.get('thermostat_id','')
 temp  = data.get('temperature',None)
-if thermostat_id != "" and temp is not None:
-   thermostat = hass.states.get(thermostat_id)
-   attributes = thermostat.attributes.copy()
-   attributes['current_temperature'] = temp
-   hass.states.set(thermostat_id, thermostat.state, attributes)
-   logger.error("current_temperature has been set for {} to {}".format(thermostat_id,temp)  
+
+logger.info("input {} : {}".format(thermostat_id,temp))
+thermostat = hass.states.get(thermostat_id)
+
+if thermostat is not None and temp is not None:
+    attributes = thermostat.attributes.copy()
+    attributes['current_temperature'] = temp
+    hass.states.set(thermostat_id, thermostat.state, attributes)
+    logger.info("current_temperature has been set for {} to {}".format(thermostat_id,temp))
 else:
-   logger.error("Wrong thermostat_id")
+    logger.error("Wrong thermostat_id")
